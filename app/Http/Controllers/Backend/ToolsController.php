28720c3d8ea26f82a14e082f271506d01170a63f
DmytroBiriukov <dmytro.biriukov@gmail.com>
2018-01-05 23:34:24 +0200

    Migrations added

---
+++ app/Http/Controllers/Backend/ToolsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\tools;
+use Illuminate\Http\Request;
+
+class ToolsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\tools  $tools
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tools $tools)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\tools  $tools
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tools $tools)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\tools  $tools
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tools $tools)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\tools  $tools
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tools $tools)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_01_05_205004_create_tools_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tool_type', 31);
+            $table->string('model_title', 31);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tools');
+    }
+}
